a073bfec2a80a516582070dab8640c184275eca1
Rajesh Kannan <tmkannanrajesh@gmail.com>
2026-06-27 14:47:28 +0530

    Added Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export const metadata: Metadata = {
   ],
 
   authors: [{ name: "Mihaela Perelighin", url: "https://www.mihaelaperelighin.com" }],
-
   creator: "Mihaela Perelighin",
 
   openGraph: {
@@ -110,6 +109,17 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-XLY2G0Y1YT');
+          `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xdikgg57vn");
           `}
         </Script>
 
